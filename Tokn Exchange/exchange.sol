ba44aebd32bd08499fb807a3f70f6491231cae2b
AnkitJaiswal1605 <85349129+AnkitJaiswal1605@users.noreply.github.com>
2021-08-06 11:18:55 +0530

    Add files via upload

---
+++ Tokn Exchange/exchange.sol
@@ -0,0 +1,92 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity ^0.8.0;
+
+import "./tokn.sol";
+
+contract Exchange is Tokn {
+    
+    struct Order {
+        uint qty;
+        uint price;
+    }
+    
+    Order[] public sellOrders;
+    Order[] public buyOrders;
+    
+    mapping(uint => address) public seller;
+    mapping(uint => address) public buyer;
+    
+        function lowestAskOrder() public view returns(uint) {
+        uint lowestAsk = sellOrders[0].price;
+        uint id = 0;
+        for(uint i = 1; i < sellOrders.length; i++) {
+            if (sellOrders[i].price < lowestAsk && sellOrders[i].qty !=0) {
+                id = i;
+            }
+        }
+        return id;
+    }    
+    
+    function highestBidOrder() public view returns(uint) {
+        uint highestBid = buyOrders[0].price;
+        uint id = 0;
+        for(uint i = 1; i < buyOrders.length; i++) {
+            if (buyOrders[i].price > highestBid && buyOrders[i].qty !=0) {
+                id = i;
+            }
+        }
+        return id;
+    }
+
+    function sellLimitOrder(uint _qty, uint _price) public {
+        require(_qty <= balances[msg.sender]);
+        uint buyId = highestBidOrder();
+        Order memory target = buyOrders[buyId];
+        if(_price <= target.price && _qty <= target.qty) {
+            balances[msg.sender] -= _qty;
+            balances[buyer[buyId]] += _qty;
+            buyOrders[buyId].qty = 0;
+        } else {
+        sellOrders.push(Order(_qty, _price));
+        uint id = sellOrders.length - 1;
+        seller[id] = msg.sender;
+        }
+    }
+    
+    function buyLimitOrder(uint _qty, uint _price) public {
+        uint sellId = lowestAskOrder();
+        Order memory target = sellOrders[sellId];
+        if(_price >= target.price && _qty <= target.qty) {
+            balances[msg.sender] += _qty;
+            balances[seller[sellId]] -= _qty;
+            sellOrders[sellId].qty = 0;
+        } else {
+        buyOrders.push(Order(_qty, _price));
+        uint id = buyOrders.length - 1;
+        buyer[id] = msg.sender;
+        }
+    }
+    
+    function sellMarketOrder(uint _qty) public {
+        require(_qty <= balances[msg.sender]);
+        uint buyId = highestBidOrder();
+        Order memory target = buyOrders[buyId];
+        if(_qty <= target.qty) {
+            balances[msg.sender] -= _qty;
+            balances[buyer[buyId]] += _qty;
+            buyOrders[buyId].qty = 0;
+        }
+    }
+    
+    function buyMarketOrder(uint _qty) public {
+        uint sellId = lowestAskOrder();
+        Order memory target = sellOrders[sellId];
+        if(_qty <= target.qty) {
+            balances[msg.sender] += _qty;
+            balances[seller[sellId]] -= _qty;
+            sellOrders[sellId].qty = 0;
+        }
+    }
+    
+}
